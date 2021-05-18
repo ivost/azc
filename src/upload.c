592ccbb98b9d806962c18f1e8363faad24287019
Ivo Stoyanov <ivostoy@gmail.com>
2021-05-18 15:40:01 -0700

    threads

--- src/upload.c
+++ src/upload.c
@@ -7,40 +7,41 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <linux/inotify.h>
+#include <sys/inotify.h>
+
 #include "upload.h"
 
-#define EVENT_SIZE  ( sizeof (struct inotify_event) )
-#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+#define EVENT_SIZE    ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN ( 1024 * ( EVENT_SIZE + 16 ) )
 
-int filewatch_init(char *dir) {
-    int fd;
-    fd = inotify_init();
-    wd = inotify_add_watch( fd, dir, IN_CREATE | IN_CLOSE_WRITE | IN_DELETE );
-    return fd;
+void filewatch_init(char *dir, int *fd, int *wd) {
+    *fd = inotify_init();
+    *wd = inotify_add_watch( *fd, dir, IN_CREATE | IN_CLOSE_WRITE | IN_DELETE );
+    printf("filewatch_init %s, fw %d, wd %d\n", dir, *fd, *wd);
 }
 
 void * upload_thread(void *ptr) {
     (void) ptr;
-    int wd;
-    char buffer[EVENT_BUF_LEN];
+    int fd, wd;
+    int numRead;
+    char buf[EVENT_BUF_LEN];
+    struct inotify_event * p_event;
+
     printf("upload_thread\n");
-    wd = filewatch_init("/home/ivo/Videos");
-    while (1) {
+    filewatch_init("/home/ivo/Videos", &fd, &wd);
+    for (;;) {
         usleep(10000);
-        ssize_t length = read( fd, buffer, EVENT_BUF_LEN );
-        while ( i > 0 && i < length ) {
-            struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
-            if ( event->len ) {
-                if ( event->mask & IN_CREATE ) {
-                    printf( "New file %s created.\n", event->name );
-                } else if ( event->mask & IN_CLOSE_WRITE ) {
-                    printf( "File %s closed.\n", event->name );
-                } else if ( event->mask & IN_DELETE ) {
-                    printf( "File %s deleted.\n", event->name );
-                }
+        numRead = read(fd, buf, EVENT_BUF_LEN);
+        for (char *p = buf; p < buf + numRead; ) {
+            p_event = ( struct inotify_event * ) p;
+            if ( p_event->mask & IN_CREATE ) {
+                printf( "New file %s created.\n", p_event->name );
+            } else if ( p_event->mask & IN_CLOSE_WRITE ) {
+                printf( "File %s closed.\n", p_event->name );
+            } else if ( p_event->mask & IN_DELETE ) {
+                printf( "File %s deleted.\n", p_event->name );
             }
-            i += EVENT_SIZE + event->len;
+            p += sizeof(struct inotify_event) + p_event->len;
         }
     }
     inotify_rm_watch( fd, wd );
